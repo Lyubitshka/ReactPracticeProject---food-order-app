dd7e08e41f18c96e7df049dfa096c4b78618bba3
Lyubitshka <antwarowska@gmail.com>
2022-12-16 21:58:45 +0100

    header button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,18 @@
 import React , { Fragment } from "react";
 
 import classes from './Header.module.css';
+import headerImg from '../../assets/header_img.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>Order your fav Food never was so simple!</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
-            <div>
-                <img />
+            <div className={classes['main-image']}>
+                <img src={headerImg} alt="Food order app"/>
             </div>
         </Fragment>
          
